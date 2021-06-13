e1cdaa13d730e809cad632edaa6ab2fbca7337c2
shahi357 <shahitank1@gmail.com>
2021-06-13 22:52:12 +0545

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bibek_app/view/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -12,54 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: HomePage(),
       
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.lightBlueAccent,
-      appBar: AppBar(
-        title: Text('Bibek App'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Enter Name'
-              ),
-            ),
-
-            SizedBox(height: 10,),
-
-                        TextField(
-              decoration: InputDecoration(
-                labelText: 'Enter Email'
-              ),
-            ),
-
-            SizedBox(height: 10,),
-
-            ElevatedButton(onPressed: (){}, 
-            child: Text('Sign Up'))
-
-          ],
-        ),
-      )
-      
-    );
-  }
-}
